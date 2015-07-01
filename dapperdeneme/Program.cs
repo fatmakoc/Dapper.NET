6f1adf3e9be49e3057635cfb07bbdd2407cdf84c
Fatma Koç <fatmakoc94@gmail.com>
2015-07-01 16:13:13 +0300

    A simple example

---
+++ dapperdeneme/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using System.Data.SqlClient;
+
+
+namespace dapperdeneme
+{
+    class Student
+    {
+        public int OgrNo { get; set; }
+        public string Adi { get; set; }
+        public string Bolum { get; set; }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sqlconnection = new SqlConnection("Data Source=FATMA\\SQLEXPRESS;Initial Catalog=OKUL;Integrated Security=True"))
+            {
+                IEnumerable<Student> students = sqlconnection.Query<Student>("SELECT * From Ogrenci where OgrNo >= @OgrNo", new { OgrNo = 12 });
+
+                foreach (var student in students )
+                Console.WriteLine("Ogrencinin Adı : {0}",student.Adi);
+            
+            }
+        }
+
+       
+    }
+}
---
+++ dapperdeneme/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dapperdeneme")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("dapperdeneme")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("461c7fc8-97b0-4409-b75d-d6a46744715a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
